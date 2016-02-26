8455de14985c9905eb2e1d4664b3c3fd8fac7fde
Zander Mackie <zmackie@gmail.com>
2016-02-26 15:57:52 -0500

    write recursion tests

---
+++ recursion.rb
@@ -0,0 +1,36 @@
+require 'pry'
+
+class Recursive
+  class << self
+    def iter_factorial(n)
+      (1..n).reduce(:*)
+    end
+
+    def recur_factorial(n)
+      return 1 if n == 0
+      return n * recur_factorial(n - 1)
+    end
+
+    def palindrome(word)
+      return true if (word.length <= 1)
+      if word[0] != word[-1]
+        return false
+      else
+        return palindrome(word[1..-2])
+      end
+    end
+
+    def power(x, n)
+      return 1 if n == 0
+      if (n > 0 && n.even?)
+        y = power(x, n/2)
+        return y * y
+      elsif (n > 0) # n is odd
+        m = n - 1
+        return x * power(x, m)
+      else # n is negegative
+        return Rational(1, power(x, -n))
+      end
+    end
+  end
+end
---
+++ test/algo_test.rb
@@ -0,0 +1,56 @@
+require 'minitest/autorun'
+require 'pry'
+require_relative '../insertion_sort'
+require_relative '../recursion'
+
+class TestInsertSort < MiniTest::Test
+
+  def test_orderered_insert
+    arr = [1, 2, 4, 5, 6]
+
+    InsertSort.sorted_insert(arr, 4, 3 )
+
+    assert_equal [1, 2, 3, 4, 5, 6], arr
+
+    array = [3, 5, 7, 11, 13, 2, 9, 6]
+
+    InsertSort.sorted_insert(array, 4, 2)
+
+    assert_equal [2, 3, 5, 7, 11, 13, 9, 6], array
+  end
+
+  def test_sort
+    myArr = Array.new(8) {rand(189)}
+
+    sorted = myArr.sort
+
+    assert_equal InsertSort.sort(myArr), sorted
+  end
+end
+
+class TestRecursive < MiniTest::Test
+
+  def test_iter_factorial
+    assert_equal 120, Recursive.iter_factorial(5)
+  end
+
+  def test_recur_factorial
+    assert_equal 120, Recursive.recur_factorial(5)
+    assert_equal 3628800, Recursive.recur_factorial(10)
+  end
+
+  def test_palindrome
+    assert Recursive.palindrome("a")
+    assert Recursive.palindrome("rotor")
+    assert !Recursive.palindrome('motor')
+  end
+
+  def test_powers
+    assert_equal Recursive.power(3, 0), 1
+    assert_equal Recursive.power(3, 1), 3
+    assert_equal Recursive.power(3, 2), 9
+    assert_equal  1/3r, Recursive.power(3, -1)
+  end
+end
+
+
