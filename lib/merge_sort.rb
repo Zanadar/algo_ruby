dd39ba6d2b1edd70297d4533a8a7a6f1e1c82cb4
Zander Mackie <zmackie@gmail.com>
2016-06-16 20:52:50 -0400

    Reimplement merge_ and quick_ sort

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -1,19 +1,28 @@
-class Merge
-  class << self
-    def sort(arr)
-      return arr if arr.length == 1
-      middle = arr.length / 2
-      left = sort(arr[0...middle])
-      right = sort(arr[middle..-1])
-      merge(left, right)
-    end
+class MergeSort
+  def initialize(arr)
+    @arr = arr
+  end
 
-    def merge(arr1, arr2)
-      result = []
-      until arr1.size == 0 || arr2.size == 0 do
-          result << (arr1[0] <= arr2[0] ? arr1.shift : arr2.shift)
+  def sort
+    return @arr if @arr.length <= 1
+    middle = @arr.length / 2
+    left, right = @arr[0...middle], @arr[middle..-1]
+
+    left = MergeSort.new(left).sort
+    right = MergeSort.new(right).sort
+
+    merge(left, right)
+  end
+
+  def merge(arr1, arr2)
+    result = []
+    while !arr1.empty? && !arr2.empty?
+      if arr1[0] <= arr2[0]
+        result << arr1.shift
+      else
+        result << arr2.shift
       end
-      result + arr1 + arr2
     end
+    result + arr1 + arr2
   end
 end
--- lib/quick_sort.rb
+++ lib/quick_sort.rb
@@ -1,43 +1,27 @@
-require 'pry'
-class Quick
+class QuickSort
   class << self
     def sort(arr)
-      return arr if arr.size <= 1
-
-      pivot_v = arr.sample
-      pivot_i = arr.index(pivot_v)
-
-      arr.slice!(pivot_i)
-
-      lesser = []
-      greater = []
-
-      arr.each do |x|
-        if x <= pivot_v
-          lesser << x
-        else
-          greater << x
-        end
-      end
-
-      sort(lesser) + [pivot_v] + sort(greater)
+      # This is in place
+      quick_sort(arr, 0, arr.length-1)
     end
 
-    def in_place(arr, lo, hi)
-      return arr unless lo < hi
-      p = partition(arr, lo, hi)
-      binding.pry if !p
-      in_place(arr, lo, p-1)
-      in_place(arr, p+1, hi)
+    def quick_sort(arr, lo, hi)
+      if lo < hi
+        p = partition(arr, lo, hi)
+        quick_sort(arr, lo, p-1)
+        quick_sort(arr, p+1, hi)
+      else
+        return arr
+      end
     end
 
     def partition(arr, lo, hi)
       pivot = arr[hi]
       i = lo
-      for j in lo..hi-1
-        if arr[j] <= pivot
-          arr[i], arr[j] = arr[j], arr[i]
-          i += 1
+      (lo...hi).each do |x|
+        if arr[x] <= pivot
+          arr[x], arr[i] = arr[i], arr[x]
+          i +=1
         end
       end
       arr[i], arr[hi] = arr[hi], arr[i]
